96b6c267b879d46bdc6a1be9488035b812903806
Michal Wojcik <wojcikmichal98@gmail.com>
2018-02-06 23:49:35 +0100

    This is where my journey begins

---
+++ README.md
@@ -0,0 +1,16 @@
+#SLEEPER
+##Purpose
+Sleeper is a device that helps you fall asleep faster by reducing your breathing form 11 to 6 breaths per minute. It lightens and dims the LED so you know when you should inhale and exhale. You can set maximum brightness of LEDs and maximum time the device will be operating. After this time it will shut down every periphery and go to power-down mode to save battery. It can operate on single LiPo cell or two AA batteries or whatever gives the right voltage for ATtiny13.
+
+It is based on the project [FADing](http://www.instructables.com/id/FADing-Fall-Asleep-Device/) but it uses smaller and cheaper ATtiny13.
+
+##Usage
+After powering up you can set maximum brightness and time for which the device will be operating. You increase brightness/time with one button and decrease with the second. Third button changes what you will modify at the moment (brightness or time). Maximum brightness is showed on one LED and time is symbolized on the other (brighter means longer). One increase in time is increase of one minute (same for decrease). After about 4 seconds from the last button push the devie will blink three times and go to operating mode. In this mode you can select which LED will be used (during all operating time). Pulsations slowly decrease from 11 to 6 pulses per minute. After the time set earlier device turns off LEDs and go to power-down mode. You can start using it again by pressing any button and then following instructons from the beggining but this time it will remember your previos settings (brightness and time).
+
+##Schematics and PCB
+Schematics and PCB are available on [CircuitMaker](https://workspace.circuitmaker.com/Projects/Details/Micha-Wjcik/sleeper).
+
+##Compiling
+Program can be compiled with Atmel Studio 7 or other similar tool.
+
+Output file has 1014 bytes which is 99% of device's memory.
---
+++ main.c
@@ -0,0 +1,149 @@
+/*
+ * sleeper.c
+ *
+ * Created: 03.02.2018 14:27:16
+ * Author : Wojcik98
+ */ 
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#define PERIOD_MS 20
+#define SECOND (1000/PERIOD_MS)
+#define MINUTE (60*SECOND)
+#define MIN_BPM (6)
+#define MAX_BPM (11)
+#define WAITING_PERIOD (4*SECOND)
+#define PRESSED(x) (!(PINB & (1<<x)))
+
+void sleep(void){
+	OCR0A = 0;
+	OCR0B = 0;
+	TCCR0B &= ~(1<<CS00) & ~(1<<CS01) & ~(1<<CS02);		// turn off timer
+	DDRB &= ~(1<<0) & ~(1<<1);	// turn off outputs
+	GIMSK |= (1<<PCIE);					// enable PCINT
+	PCMSK |= (1<<2) | (1<<3) | (1<<4);	// ...on pins PB2..4
+	SREG |= (1<<7);			// enable interrupts
+	MCUCR |= (1<<SE);	// enable sleep
+	asm("sleep");
+}
+
+int main(void)
+{
+	uint8_t maxTimeMins = 8, maxBright = 100, prevState = 0, bright = 0;
+	uint8_t bpm = MAX_BPM;
+	uint16_t time = 0, last = 0, period = (60*SECOND)/bpm;
+	uint8_t mode = 0;
+
+	// SETUP
+	DDRB = (1<<1) | (1<<0);	// pb0 and 1 as outputs (led pwm), rest as inputs
+	TCCR0A |= (1<<WGM02) | (1<<WGM01) | (1<<WGM00);	// fast PWM
+	TCCR0A |= (1<<COM0A1) | (1<<COM0B1);		// non-inverted PWM
+	TCCR0B |= (1<<CS00);				// set internal clock as timer clock
+	MCUCR |= (1<<SM1);	// sleep will enter power-down mode
+	PRR |= (1<<PRADC);	// turn off adc
+
+	while(1){
+		if(PRESSED(2)){			// changing mode
+			mode = (prevState&(1<<2)) ? (mode) : (mode ^ 1);
+			prevState |= (1<<2);
+		}else{
+			prevState &= ~(1<<2);
+		}
+
+		if(time < WAITING_PERIOD){	// setting brightness and time
+			//mode &= ~(1<<1);
+			if(PRESSED(3)){		// increase time or brightness
+				if(mode == 0)	// add if button wasn't pressed in last cycle
+					maxBright = (maxBright < 245) ? (maxBright + 5*(!(prevState & (1<<3)))) : 255;	// bad steering near 255, but it won't be visible anyway
+				else
+					maxTimeMins = (maxTimeMins < 21) ? (maxTimeMins + !(prevState & (1<<3))) : maxTimeMins;
+				prevState |= (1<<3);
+				time = 0;
+			}else{
+				prevState &= ~(1<<3);
+			}
+			if(PRESSED(4)){		// decrase...
+				if(mode == 0)
+					maxBright = (maxBright - 5*(!(prevState & (1<<4))) >= 0) ? (maxBright - 5*(!(prevState & (1<<4)))) : 0;
+				else
+					maxTimeMins = (maxTimeMins > 1) ? (maxTimeMins - !(prevState & (1<<4))) : maxTimeMins;
+				prevState |= (1<<4);
+				time = 0;
+			}else{
+				prevState &= ~(1<<4);
+			}
+			OCR0A = maxTimeMins*5;
+			OCR0B = maxBright;
+		}
+		else if(time == WAITING_PERIOD){	// blinking
+			OCR0A = 150;
+			OCR0B = 150;
+			_delay_ms(255);
+			OCR0A = 0;
+			OCR0B = 0;
+			_delay_ms(255);
+			OCR0A = 150;
+			OCR0B = 150;
+			_delay_ms(255);
+			OCR0A = 0;
+			OCR0B = 0;
+			_delay_ms(255);
+			OCR0A = 150;
+			OCR0B = 150;
+			_delay_ms(255);
+			OCR0A = 0;
+			OCR0B = 0;
+			_delay_ms(255);
+			last = time;
+		}
+		else{					// pulsing light
+			if(time%(MINUTE) == 0){				// decrease bpm
+				bpm = (bpm>MIN_BPM) ? (bpm - 1) : (bpm);
+			}
+
+			if(last + ((period+1)>>1) > time){	// lighten up
+				uint8_t pwm = ((time - last) * (maxBright)) / (period>>1);
+				bright = (pwm > maxBright) ? maxBright : pwm;
+			}else{								// darken down
+				uint8_t pwm = ((last + period - time) * (maxBright)) / (period>>1);
+				bright = (pwm > maxBright) ? maxBright : pwm;
+			}
+
+			if(last + period <= time){			// start new cycle
+				period = (60*SECOND)/bpm;
+				last = time;
+				bright = 0;
+			}
+
+			if(!(mode&1)){			// light proper LED
+				DDRB &= ~(1<<1);
+				DDRB |= (1<<0);
+				OCR0A = bright;
+			}else{
+				DDRB |= (1<<1);
+				DDRB &= ~(1<<0);
+				OCR0B = bright;
+			}
+		}
+
+		if(time >= maxTimeMins*MINUTE){	// sleep after maximum time is achieved
+			time = 0;
+			bpm = MAX_BPM;
+			prevState = 255;
+			sleep();
+		}
+
+		_delay_ms(PERIOD_MS);	// loop roughly 50Hz, but we don't care that much for precision and we don't have more timers
+		time++;
+	}
+}
+
+ISR(PCINT0_vect){			// interrupt vector
+	GIMSK &= ~(1<<PCIE);	// disable PCINT
+	PCMSK = 0;				// disable interrupts inputs
+	MCUCR &= ~(1<<SE);		// disable sleep
+	TCCR0B |= (1<<CS00);	// turn on timer
+	DDRB = (1<<1) | (1<<0);	// set as outputs
+}
